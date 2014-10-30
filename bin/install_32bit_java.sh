5cf5f339fb07f6553aeb9cfedd96632627002972
Matthew Blair <matthew.blair@navy.mil>
2014-10-30 08:20:45 -0400

    Automate extraction of 32-bit Oracle jre

--- bin/install_32bit_java.sh
+++ bin/install_32bit_java.sh
@@ -8,10 +8,30 @@
 # 3. Update /usr/java/latest/jre/bin/java (which is a shell script) to reference
 # the correct path for 32-bit jre 
 #
-# Future improvement: automate steps 1 and 2
+
+echo off
+
+# Must be run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "This setup tool must be run as root.  Exiting..." 1>&2
+    exit 1
+fi
+
+# User must have provided file location for 32-bit jre
+if [ ! -f "$1" ]; then
+    echo "Usage: install_32bit_java PATH_TO_INSTALL_FILE" 1<&2
+    exit 1
+fi
+
+echo "Unpacking $1 to /opt/java32"
+mkdir /tmp/java32
+tar xvf $1 -C /tmp/java32
+mv /tmp/java32/* /opt/java32/
 
 # This is Step 3 - change /usr/java/latest/jre/bin/java to reference the correct path for 32-bit jre
 
 JRE_VERSION="$(ls /opt/java32)"
-
+echo $JRE_VERSION
 sed -i "s/jre[0-9].*_.*\//$JRE_VERSION\//" /usr/java/latest/jre/bin/java 
+
+echo "32-bit JRE install complete!"
