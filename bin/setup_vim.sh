99679f9af1969c69d1e049c872355801680b4bdc
Matthew Blair <matthew.blair@navy.mil>
2014-11-11 12:43:55 -0500

    Fixed path issue for .vimrc, added progress output, and added step to run Helptags

--- bin/setup_vim.sh
+++ bin/setup_vim.sh
@@ -9,14 +9,27 @@ command -v curl >/dev/null 2>&1 || { echo >&2 "curl is not installed.  Aborting.
 rm -f $HOME/.vimrc
 rm -rf $HOME/.vim
 
-ln -s $PWD/.vimrc $HOME/.vimrc
+ln -s $PWD/../.vimrc $HOME/.vimrc
 
 # create directories for pathogen and pathogen bundles
+echo "Creating directories..."
 mkdir -p $HOME/.vim/bundle
 mkdir -p $HOME/.vim/autoload
+echo "Done creating directories"
 
 # get pathogen
+echo "Retrieving pathogen via curl"
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo "Done retrieving pathogen"
 
 # NERDtree
+echo "Retrieving bundle NERDtree"
 git clone https://github.com/scrooloose/nerdtree.git $HOME/.vim/bundle/nerdtree
+echo "Done retrieving NERDtree"
+
+# run Helptags
+echo "Running Helptags"
+vim -c ":Helptags" -c ":q" >&/dev/null
+echo "Done running Helptags"
+
+echo "vim setup complete!"
